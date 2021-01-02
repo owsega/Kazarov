5e7c1724524c6fb865eba6a70eda5dfd2b0caf7c
gabrielowoeye <owoeye.g.o@gmail.com>
2021-01-02 15:58:59 +0100

    add main activity UI

---
+++ app/src/main/java/dev/owsega/kazarovdelivery/HeaderAdvertFragment.kt
@@ -0,0 +1,36 @@
+package dev.owsega.kazarovdelivery
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import dev.owsega.kazarovdelivery.databinding.HeaderViewpagerItemBinding
+
+class HeaderAdvertFragment : Fragment() {
+    private var _binding: HeaderViewpagerItemBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = HeaderViewpagerItemBinding.inflate(inflater, container, false)
+        binding.headerVpItemTitle.text = arguments?.getString(Intent.EXTRA_TITLE)
+        binding.headerVpItemSubtitle.text = arguments?.getString(Intent.EXTRA_TEXT)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        const val TAG = "HeaderAdvertFragment"
+        fun newInstance(data: MainActivity.HeaderPageData) = HeaderAdvertFragment().apply {
+            arguments = Bundle().apply {
+                putString(Intent.EXTRA_TITLE, data.title)
+                putString(Intent.EXTRA_TEXT, data.subtitle)
+            }
+        }
+    }
+}
--- app/src/main/java/dev/owsega/kazarovdelivery/MainActivity.kt
+++ app/src/main/java/dev/owsega/kazarovdelivery/MainActivity.kt
@@ -1,11 +1,44 @@
 package dev.owsega.kazarovdelivery
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import dev.owsega.kazarovdelivery.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // set appbar to about 70% of the screen height
+        val heightDp = (resources.displayMetrics.heightPixels / 1.4).toFloat()
+        val lp = binding.appbar.layoutParams as CoordinatorLayout.LayoutParams
+        lp.height = heightDp.toInt()
+
+        val dummyHeaderData = listOf(
+            HeaderPageData("Saturday Discount", "Coca-cola is a gift to any order"),
+            HeaderPageData("Tuesday Discount", "Coca-cola is a gift to any order"),
+            HeaderPageData("Monday Discount", "Coca-cola is a gift to any order"),
+        )
+
+        binding.header.headerViewpager.adapter = HeaderPagerAdapter(dummyHeaderData, supportFragmentManager)
+        binding.header.dotsIndicator.setViewPager(binding.header.headerViewpager)
+        binding.header.headerViewpager.adapter?.registerDataSetObserver(binding.header.dotsIndicator.dataSetObserver)
     }
+
+    internal class HeaderPagerAdapter(private val pages: List<HeaderPageData>, fm: FragmentManager) :
+        FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+        override fun getItem(position: Int) = HeaderAdvertFragment.newInstance(pages[position])
+        override fun getPageTitle(position: Int) = pages[position].title
+        override fun getCount() = pages.size
+    }
+
+    data class HeaderPageData(val title: String, val subtitle: String)
 }
